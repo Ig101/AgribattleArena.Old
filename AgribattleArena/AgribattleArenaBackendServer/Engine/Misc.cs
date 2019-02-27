edc8716291afe1803faa8bad2075dc81ab6f3a1b
Ig101 <nsn1001@mail.ru>
2019-02-27 20:24:21 +0300

    Add model classes

---
+++ AgribattleArena/AgribattleArenaBackendServer/Engine/Misc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgribattleArenaBackendServer.Engine
+{
+    public delegate void SceneTileAction(Scene scene, Tile tile, float mod);
+    public delegate void SceneObjectAction(Scene scene, Tile tile, float mod);
+    public class Misc
+    {
+        
+    }
+}
---
+++ AgribattleArena/AgribattleArenaBackendServer/Engine/Tile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgribattleArenaBackendServer.Engine
+{
+    public class Tile
+    {
+        int x;
+        int y;
+        string name;
+
+        GameObject decorations;
+
+    }
+}
