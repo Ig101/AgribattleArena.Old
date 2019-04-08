63f271009a9354836a284f0cbaf3c1cab7b7d197
Ig101 <nsn1001@mail.ru>
2019-04-08 19:39:03 +0300

    Change projects' names

--- AgribattleArena/AgribattleArenaBackendServer/Program.cs
+++ AgribattleArena/AgribattleArenaBackendServer/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace AgribattleArenaBackendServer
+namespace AgribattleArena.BackendServer
 {
     public class Program
     {
--- AgribattleArena/AgribattleArenaBackendServer/Startup.cs
+++ AgribattleArena/AgribattleArenaBackendServer/Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace AgribattleArenaBackendServer
+namespace AgribattleArena.BackendServer
 {
     public class Startup
     {
