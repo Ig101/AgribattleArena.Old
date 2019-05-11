ce6e81f4c1f90071b8a5b70aece00ddb44b879fc
Ig101 <nsn1001@mail.ru>
2019-05-11 21:52:16 +0300

    Implement common services

--- AgribattleArena/AgribattleArena.BrowserClient/AgribattleArenaClient/src/app/app.module.ts
+++ AgribattleArena/AgribattleArena.BrowserClient/AgribattleArenaClient/src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { TOASTR_TOKEN, JQ_TOKEN } from './common/index';
+
+// tslint:disable-next-line: no-string-literal
+const toastr = window['toastr'];
+// tslint:disable-next-line: no-string-literal
+const jQuery = window['$'];
 
 @NgModule({
   declarations: [
   ],
   imports: [
     BrowserModule
-    
   ],
   providers: [
-
+    {provide: TOASTR_TOKEN, useValue: toastr},
+    {provide: JQ_TOKEN, useValue: jQuery}
   ],
   bootstrap: [
 
