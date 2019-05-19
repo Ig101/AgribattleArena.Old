3186bbe5aba35747f0e3bcb18f1b4e3f6b289130
Ig101 <nsn1001@mail.ru>
2019-05-19 23:33:42 +0300

    Add start validation

--- AgribattleArena/AgribattleArena.BrowserClient/AgribattleArenaClient/src/app/main/authorize/register.component.ts
+++ AgribattleArena/AgribattleArena.BrowserClient/AgribattleArenaClient/src/app/main/authorize/register.component.ts
@@ -1,26 +1,42 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
 import { AuthService } from '../../share/index';
 import { LoadingService } from 'src/app/loading';
 import { IExternalWrapper } from 'src/app/share/models';
 import { checkServiceResponseError, getServiceResponseErrorContent } from 'src/app/common';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-register',
     templateUrl: './register.component.html',
     styleUrls: ['../main.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
 
     @Output() cancelEmitter = new EventEmitter();
     @Output() registerEmitter = new EventEmitter();
 
-    private userName;
-    private email;
-    private password;
-    private confirmPassword;
+    private registerForm: FormGroup;
+    private userName: FormControl;
+    private email: FormControl;
+    private password: FormControl;
+    private confirmPassword: FormControl;
 
     constructor(private authService: AuthService, private loadingService: LoadingService) {
 
+    }
+
+    ngOnInit() {
+        this.userName = new FormControl('', [Validators.required, Validators.pattern('^[A-Za-z]+[A-Za-z0-9]*.{2,}$')]);
+        this.email = new FormControl('', [Validators.required, Validators.email]);
+        this.password = new FormControl('', [Validators.required,
+            Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z0-9$@$!%*?&].{5,}')]);
+        this.confirmPassword = new FormControl('', [Validators.required]);
+        this.registerForm = new FormGroup({
+          userName: this.userName,
+          email: this.email,
+          password: this.password,
+          confirmPassword: this.confirmPassword
+        });
     }
 
     registerButtonPress(formValue) {
