4bd5042f578ec10cffc0057ca8e93324d36c8098
Ig101 <nsn1001@mail.ru>
2019-05-26 15:23:57 +0300

    Add child routes to main

--- AgribattleArena/AgribattleArena.BrowserClient/AgribattleArenaClient/src/app/main/main.routes.ts
+++ AgribattleArena/AgribattleArena.BrowserClient/AgribattleArenaClient/src/app/main/main.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './main.component';
 import { MainResolver } from './main.resolver';
+import { StartPageComponent } from './start-page.component';
+
+const mainChildRoutes: Routes = [
+    {path: '', component: StartPageComponent}
+];
 
 export const mainRoutes: Routes = [
-    {path: '', component: MainComponent, resolve: {profile: MainResolver}},
+    {path: '', component: MainComponent, resolve: {profile: MainResolver}, children: mainChildRoutes},
 ];
