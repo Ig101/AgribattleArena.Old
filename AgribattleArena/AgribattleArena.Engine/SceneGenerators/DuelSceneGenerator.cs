e30d31d725cd63e88ab426010f8b56f82002b5c9
Ig101 <nsn1001@mail.ru>
2019-07-07 16:17:11 +0300

    Implement duel scene generator

--- AgribattleArena/AgribattleArena.Engine/SceneGenerators/DuelSceneGenerator.cs
+++ AgribattleArena/AgribattleArena.Engine/SceneGenerators/DuelSceneGenerator.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using AgribattleArena.Engine.ForExternalUse.Generation.ObjectInterfaces;
+using AgribattleArena.Engine.Helpers;
+using AgribattleArena.Engine.Objects;
 
 namespace AgribattleArena.Engine.SceneGenerators
 {
@@ -19,17 +22,49 @@ namespace AgribattleArena.Engine.SceneGenerators
 
         public void GenerateNewScene(ISceneForSceneGenerator scene, IEnumerable<IPlayer> players, int seed)
         {
-
+            Tile[][] sceneTiles = scene.SetupEmptyTileSet(19, 19);
+            for (int x = 0; x < sceneTiles.Length; x++)
+            {
+                for (int y = 0; y < sceneTiles[x].Length; y++)
+                {
+                    scene.CreateTile("tile_grass", x, y, null);
+                }
+            }
+            List<IPlayer> tempPlayers = players.ToList();
+            if (tempPlayers.Count != 2) throw new ArgumentException("Players count should be 2", "players");
+            for (int i = 0; i < 2; i++)
+            {
+                if (tempPlayers[i].KeyActorsGen.Count != 5)
+                    throw new ArgumentException("Actors count should be 5. Thrown on player " + tempPlayers[i].Id, "players.keyActors");
+                Player tempScenePlayer = GeneratorHelper.ConvertExternalPlayerFromGeneration(scene, tempPlayers[i], i);
+                for (int j = 0; j < tempPlayers[i].KeyActorsGen.Count; j++)
+                {
+                    tempScenePlayer.KeyActors.Add(GeneratorHelper.ConvertExternalActorFromGeneration(scene, tempScenePlayer,
+                        sceneTiles[1 + i * 16][1 + j * 4], tempPlayers[i].KeyActorsGen[j], null));
+                }
+            }
         }
 
         public static bool DefeatConditionDuel (ISceneParentRef scene, IPlayerParentRef player)
         {
-            return false;
+            foreach (Actor actor in player.KeyActors)
+            {
+                if (actor.IsAlive) return false;
+            }
+            return true;
         }
 
         public static bool WinConditionDuel (ISceneParentRef scene)
         {
-            return false;
+            int countOfRemainedPlayers = 0;
+            foreach (Player player in scene.Players)
+            {
+                if (player.Status == PlayerStatus.Playing)
+                {
+                    countOfRemainedPlayers++;
+                }
+            }
+            return countOfRemainedPlayers <= 1;
         }
     }
 }
